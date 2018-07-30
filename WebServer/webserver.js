4bbdcadb8f619a2579fc7aaff531ef5efc988056
= <wraighty@gmail.com>
2018-07-30 22:23:46 +0100

    Very early webserver, picking up files and dislay names in table

--- WebServer/webserver.js
+++ WebServer/webserver.js
@@ -1,21 +1,34 @@
-var http = require('http').createServer(handler);
+var http = require('http').createServer(LoadPage);
 var fs = require('fs');
 
+var imageDir = '/home/pi/CameraCaptures/';
+console.log('Looking in ' + imageDir);
 http.listen(8080);
+console.log('Ready to load images');
 
-function handler(req, res)
-{
-	fs.readFile('/home/pi/Documents/CameraTrap/WebServer/index.html',
-	function(err, data)
+function LoadPage(req, res)
+{		
+	res.writeHead(200, {'Content-Type' : 'text/html'}); 
+	var html = '<!DOCTYPE html><html><body><h1>Camera Trap</h1><table style="width:100%">';
+	html += '<tr><th>Image</th><th>Location</th></tr>';
+	
+	fs.readdir(imageDir, function(err, fileNames)
 	{
-		if(err)
+		console.log('Found '+ fileNames.length + ' images');
+		for(i=0;i<fileNames.length;i++)
 		{
-			res.writeHead(404, {'Content-Type' : 'text/html'});
-			return res.end(err.message); // "404 Not Found");
+			html += '<tr><td>TODO Thumb Image</td><td>' + fileNames[i] + '</td></tr>';
 		}
-		
-		res.writeHead(202, {'Content-Type' : 'text/html'}); 
-		res.write(data);
-		return res.end();
+
+	html += '</table></body></html>'
+
+	console.log('Returning ' + html);
+	return res.end(html, 'utf-8');
+
 	});
+
+		
 }
+
+
+
