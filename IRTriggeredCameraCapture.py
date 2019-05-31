56cf7b6b07b73ac81b5dc7d07298fab2e41ba2b5
jawFrome <wraighty@gmail.com>
2019-05-31 18:56:14 +0100

    Added Database storage and change filename to use dateTime

--- IRTriggeredCameraCapture.py
+++ IRTriggeredCameraCapture.py
@@ -1,3 +1,4 @@
+import datetime
 from picamera import PiCamera
 import os.path
 from time import sleep
@@ -6,6 +7,8 @@ from gpiozero import LED
 from gpiozero import DigitalInputDevice
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
+
+import sqlite3
 
 # Only need remotely
 #factory = PiGPIOFactory(host='192.168.1.87')
@@ -21,7 +24,14 @@ storageLocation = '/home/pi/CameraCaptures/'
 if os.path.exists(storageLocation) != True:
     mkdir(storageLocation)
 
-count = 0  
+# Check Database
+databaseName = 'CapturedImages.db'
+database = sqlite3.connect(storageLocation + databaseName)
+cursor = database.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS
+                      images(id INTEGER PRIMARY KEY, CaptureTime DATETIME, filePath TEXT)''') 
+database.commit()
+
 while True:
     value = IRSensor.value
     print("Value is ", value)
@@ -31,10 +41,18 @@ while True:
         sleep(1)
 
         # Take the picture
-        nextPicture = storageLocation + 'test' + str(count) + '.jpg'
+        captureTime = datetime.datetime.now()
+        nextPicture = storageLocation + 'Capture' + captureTime.strftime("%m%d%Y%H%M%S") + '.jpg'
         camera.rotation = 180
-        sleep(5)
         camera.capture(nextPicture)
+
+        #Reference it in the database
+        cursor.execute('''INSERT INTO images(CaptureTime, filePath) VALUES(?,?)''', (captureTime, nextPicture))
+        database.commit()
+
+        # Display how many captures we have
+        count = cursor.execute('SELECT Count(id) FROM images').fetchone()[0]
+        print('We have ' + str(count) + ' images')
     
         led.off()
         sleep(1)
